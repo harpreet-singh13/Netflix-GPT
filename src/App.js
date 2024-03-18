639422bb7dbd29c05c1c4539f8b492859487f935
Harpreet Singh <01sharpreet@gmail.com>
2024-03-19 01:09:37 +0530

    second

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="text-3xl font-bold text-green-600">
-      Waheguru ji Blessed Me!!
+      Waheguru ji Blessed Me!! 
+      Tu hi hain Waheguru
     </div>
+
+
   );
 }
 
